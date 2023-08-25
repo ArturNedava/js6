b611e47529a0cd7cbdb98728270b90ce8f74ee37
ArturNedava <121451856+ArturNedava@users.noreply.github.com>
2023-08-25 16:35:44 +0200

    First commit

---
+++ lando.js
@@ -0,0 +1,74 @@
+//let number = 1;
+//while (number <= 10) {
+  //  console.log(number);
+   // number++;
+//}
+
+
+//for (let number = 2; number <= 20; number += 2) {
+ //   if (number % 2 !== 0) {
+   //     continue; 
+  //  }
+ //   console.log(number);
+//}
+
+
+//for (let number = 7; number <=70; number +=7) {
+  //  console.log(number)
+//}
+
+
+
+//let numbers = [1, 2, 3, 4, 5];
+//let index= 0;
+
+//while(index < numbers.length){
+ //  console.log(numbers[index]);
+  //  index++;
+//}
+
+
+
+//let numberss = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//for(let i = 0; i < numberss.length; i++){
+
+ //  if(numberss[i] === 7){
+  //      console.log("нет семи")
+  //      break;
+   // }
+  //  console.log(numberss[i]);
+//}
+
+
+
+//let n = 10
+
+//for(let i = 1; i <n; i++){
+  //  console.log(i);
+
+  //  if(i >= n){
+   //     break;
+   // }
+//}
+
+
+
+let num = 1;
+
+while(num <= 20){
+    console.log(num)
+
+    if(num % 3 ===0 ){
+       num++
+        continue
+    }
+
+    console.log(num)
+num++
+}
+
+
+
+
+
